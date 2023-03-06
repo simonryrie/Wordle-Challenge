5d459d38804a9bba5e80e5ff29c06191b7f34986
simonryrie <simonryrie@gmail.com>
2023-03-06 21:17:29 +0000

    API fetch added

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const submitButton = document.getElementById("submit")
 submitButton.addEventListener("click", checkSubmittedWord)
 
-function getSubmittedWord(event) {
-    event.preventDefault()
+function getSubmittedWord() {
     const input = document.getElementById("word")
     const submittedWord = input.value
     input.value =  ""
     return submittedWord
 }
 
+async function checkSubmittedWord(event) {
+    event.preventDefault()
+    const word = getSubmittedWord()
+    try {
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+    catch(error) {
+        console.log(error);
+        window.alert("This is not a real word!");
+    }
+}
